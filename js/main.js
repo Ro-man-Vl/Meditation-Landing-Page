ead70cb7f1905aec23403c18729218c8f89d8f08
Ro-man-Vl <roman.itmail@gmai.com>
2022-01-18 23:38:30 +1100

    Header, Hero and Learn more slider done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.learn-more__slick-slider').slick({
+        infinite: true,
+        arrows: true,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      });
+
+});
