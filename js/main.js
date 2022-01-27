0632f470f81ca40da0b5704c7bc27e87fa7680ff
Ro-man-Vl <roman.itmail@gmai.com>
2022-01-27 18:04:15 +1100

    Header menu bounce effect added

--- js/main.js
+++ js/main.js
@@ -6,5 +6,11 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 1
       });
-
+    // Menu bounce ---------------------------------------------------------------
+    $('.header__menu-item').hover(function() {
+      if (!$(this).hasClass('header__menu-item-active')) {
+        $(this).toggleClass('bounce');
+        $(this).siblings('.header__menu-item-active').toggleClass('current')
+      }
+    })
 });
