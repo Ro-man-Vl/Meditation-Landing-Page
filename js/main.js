7e622751333a8fc53dbe5f3e9a652845127a657c
Ro-man-Vl <roman.itmail@gmai.com>
2022-02-03 17:02:28 +1100

    Yoga tabs animation done

--- js/main.js
+++ js/main.js
@@ -1,16 +1,40 @@
-$(document).ready(function(){
-    $('.learn-more__slick-slider').slick({
-        infinite: true,
-        arrows: true,
-        dots: true,
-        slidesToShow: 3,
-        slidesToScroll: 1
-      });
-    // Menu bounce ---------------------------------------------------------------
-    $('.header__menu-item').hover(function() {
-      if (!$(this).hasClass('header__menu-item-active')) {
-        $(this).toggleClass('bounce');
-        $(this).siblings('.header__menu-item-active').toggleClass('current')
-      }
-    })
+$(document).ready(function () {
+  $('.learn-more__slick-slider').slick({
+    infinite: true,
+    arrows: true,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
+  // Menu bounce ---------------------------------------------------------------
+  $('.header__menu-item').hover(function () {
+    if (!$(this).hasClass('header__menu-item-active')) {
+      $(this).toggleClass('bounce');
+      $(this).siblings('.header__menu-item-active').toggleClass('current')
+    }
+  });
+  // Yoga-tab transformation----------------------------------------------------
+  $('.yoga__tab').click(function () {
+    const $height = $('.yoga__tabs-wrapper').height();
+
+    $(this).addClass('active');
+    $('.yoga').css('--height', $height + 'px');
+   
+    $(this).siblings('.yoga__tab').hide();
+    $(this).children().addClass('active');
+
+    $('.yoga__tab-closer').css('display', 'flex');
+    
+    $('.yoga__tab-closer').click(function () {
+      $(this).siblings('.active').children().removeClass('active');
+      $(this).siblings().show();
+      $(this).siblings().removeClass('active');
+      $('.yoga').css('--height', '');
+      $(this).css('display', '');
+    });
+
+
+  });
+
+
 });
